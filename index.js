859b988cf381cd3cda99e06622b96e78d4cdbb7d
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-05-19 23:29:35 +0600

    Project Initialized with Database Setup

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 3000
+
+const app = express();
+
+// TechGigs
+// VgpJJmMHcoPMP0jZ
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER }:${process.env.DB_PASSWORD}@cluster0.1k8uoge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // const tasksCollection = client.db('TechGigs').collection('tasks');
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req,res)=>{
+    res.send("TechGigs server is running")
+    
+})
+
+app.listen(port , ()=>{
+    console.log(`TechGigs server is listening to port ${port}`)
+})
